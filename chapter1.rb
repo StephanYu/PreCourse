2a4825209f11b772e4a4c4db1476a6536d41f321
Stephan Yu <stephan.yu@gmail.com>
2014-05-13 12:41:14 +0200

    Merge remote-tracking branch 'origin/master' merge necessary to upload exercises from Pine book to Github.

---
+++ chapter1.rb
@@ -0,0 +1,39 @@
+#Write a program which tells you: how many hours are in a year? 
+def hours_in_a_year
+  year = 1
+  days_in_a_year = 365
+  hours_per_day = 24
+  puts year * days_in_a_year * hours_per_day
+end
+hours_in_a_year
+
+
+#how many minutes are in a decade? 
+def minutes_in_a_decade
+  puts 12 * 365 * 24 * 60
+end
+minutes_in_a_decade
+
+
+#how many seconds old are you? 
+def my_age
+  34 * 365 * 24 * 60 * 60
+end
+my_age
+
+
+#If I am 1031 million seconds old, how old am I?
+def how_old
+  1_031_000_000 / 60 / 60 / 24 / 365  
+end
+how_old
+
+
+
+
+
+
+
+
+
+
